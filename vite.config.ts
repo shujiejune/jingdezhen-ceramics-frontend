975b5a085076af920c2757bd580ba61aa428aded
shujiejune <jieniu.june@tutanota.com>
2025-08-12 09:24:18 -0700

    feat: config and  dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
-import solid from 'vite-plugin-solid';
+import { defineConfig } from "@solidjs/start/config";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [solid()],
+  vite: {
+    plugins: [
+      tanstackRouter({
+        routesDirectory: "./src/routes",
+        generatedRouteTree: "./src/routeTree.gen.ts",
+        autoCodeSplitting: true,
+      }),
+    ],
+  },
 });
